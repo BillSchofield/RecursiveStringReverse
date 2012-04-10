fad09f9b240d1d5e15d5ba32cd0f26aaa7630adc
Bill Schofield <bill@billschofield.net>
2012-04-10 11:50:21 -0500

    Stubbed call of reverseString

---
+++ src/stringReverse/StringReverseMain.java
@@ -0,0 +1,14 @@
+package stringReverse;
+
+public class StringReverseMain {
+
+	public static void main(String[] args) {
+		
+		String originalString = "sdrawkcaB";
+		
+		String reversedString = new StringReverser().reverse(originalString);
+		System.out.println(originalString + "reverseds is:" + reversedString);
+		
+	}
+
+}
---
+++ src/stringReverse/StringReverser.java
@@ -0,0 +1,9 @@
+package stringReverse;
+
+public class StringReverser {
+
+	public String reverse(String originalString) {
+		return null;
+	}
+
+}
